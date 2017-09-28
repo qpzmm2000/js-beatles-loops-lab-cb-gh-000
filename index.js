aec7345afd5c1723435d259f0ac2993622f6e154
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 07:28:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function johnLennonFacts(facts)
   }
   return facts
 }
+
+function iLoveTheBeatles(number){
+  var array = [];
+  do{
+    array.push("I love the Beatles!");
+    number++;
+  }while(number < 15)
+}
