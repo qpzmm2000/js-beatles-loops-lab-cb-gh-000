867d0afc9d540514aae017acbb257fecabc41e34
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 07:23:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments)
   var array = [];
   for(var i = 0; i < musicians.length; i++)
   {
-    array[musicians] = instruments
+    array.push(musicians, 'plays', instruments);
   }
   return array
 }
